d1e525ea908ca97563e67865f9d30f84c6818d6b
pasha4uu <pasha7864u@gmail.com>
2018-11-21 10:40:32 +0530

    Initial Commit

---
+++ SocketTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocketTask
+//
+//  Created by PASHA on 21/11/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocketTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocketTask
+//
+//  Created by PASHA on 21/11/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
